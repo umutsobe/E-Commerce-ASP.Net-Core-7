566ee151b25dd908549a686ed6a4d08f8a68a66c
umutsobe <umutsobee@gmail.com>
2023-07-13 02:22:10 +0300

    first

---
+++ Infastructure/e-trade-api.Persistence/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using e_trade_api.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_trade_api.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ETradeApiDBContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString())
+            );
+        }
+    }
+}
